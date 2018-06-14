acf1627e0c0c1b7c4a5e1344e0726f4ba19180b6
Dave <kingdavehangzhou@gmail.com>
2018-06-14 19:41:22 +0800

    init project!

---
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dave
+ *
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello spring security";
+	}
+
+}
